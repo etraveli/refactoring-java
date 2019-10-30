0238e988b4d9eac28b93eadc0946e23750213ddd
Lundell <=>
2019-10-30 15:32:59 +0100

    Added Movies as singleton, a stock for all Movies. Could be called MovieStock(?)

---
+++ src/Movies.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Movies {
+
+    private static Movies INSTANCE;
+    private static HashMap<String, Movie> movies;
+
+    private Movies() {
+        movies = new HashMap();
+        movies.put("F001", new Movie("You've Got Mail", PriceRange.REGULAR));
+        movies.put("F002", new Movie("Matrix", PriceRange.REGULAR));
+        movies.put("F003", new Movie("Cars", PriceRange.CHILDREN));
+        movies.put("F004", new Movie("Fast & Furious X", PriceRange.NEW));
+    }
+
+    public static Movies getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new Movies();
+        }
+
+        return INSTANCE;
+    }
+
+    public static Movie getMovieByCode(String code) {
+        return movies.get(code);
+    }
+}
