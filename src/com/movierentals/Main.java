422c174418cbf7143ab3a532f0e4b850edf65056
Vasilis Karagiorgis <vasilis.karagiorgis@gmail.com>
2019-11-14 17:48:09 +0100

    Use final variables to indicate no changes and flipped the equals check
    - To avoid potential NPEs. The result could be null and by putting it
    in the right hand side we make sure that the code does not throw NPE.

--- src/com/movierentals/Main.java
+++ src/com/movierentals/Main.java
@@ -6,15 +6,15 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer" + System.lineSeparator()
+    final String expected = "Rental Record for C. U. Stomer" + System.lineSeparator()
                     + "\tYou've Got Mail\t3.5" + System.lineSeparator()
                     + "\tMatrix\t2.0" + System.lineSeparator()
                     + "Amount owed is 5.5" + System.lineSeparator()
                     + "You earned 2 frequent points" + System.lineSeparator();
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    final String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
 
-    if (!result.equals(expected)) {
+    if (!expected.equals(result)) {
       throw new AssertionError("Expected: " + System.lineSeparator() + expected + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
     }
 
