55f3fb9e1971004a4c0cd59caa0533eb614df7bd
EjazuddinSyed <70020569+EjazuddinSyed@users.noreply.github.com>
2020-08-21 15:38:03 +0200

    Muhammad Ejazuddin Syed
    Changes mentioned in comments

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,44 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
-  public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+	public static void main(String[] args) {
+		String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+	
+		//Initializing customer 
+		Customer customer = customerCreation();
+		
+		//Initializing movies 
+		HashMap<String, Movie> movies = moviesCreation();
+		
+		//Adding movies as function argument to make inputs in a single place
+		String result = new RentalInfo().statement(customer, movies);
+	
+	    if (!result.equals(expected)) {
+	    throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+	    }
+	
+	    System.out.println("Success");
+	}
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
-
-    if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
-    }
-
-    System.out.println("Success");
-  }
+	//Making functions for input for easy readability 
+	private static Customer customerCreation() {
+		MovieRental movieRental1 = new MovieRental("F001", 3);
+		MovieRental movieRental2 = new MovieRental("F002", 1);
+		List<MovieRental> movieRentals = Arrays.asList(movieRental1, movieRental2);
+		Customer customer = new Customer("C. U. Stomer", movieRentals);
+		return customer;
+	}
+	
+	private static HashMap<String, Movie> moviesCreation() {
+		HashMap<String, Movie> movies = new HashMap<String, Movie>();
+	    movies.put("F001", new Movie("You've Got Mail", "regular"));
+	    movies.put("F002", new Movie("Matrix", "regular"));
+	    movies.put("F003", new Movie("Cars", "childrens"));
+	    movies.put("F004", new Movie("Fast & Furious X", "new"));
+		return movies;
+	}
+	
 }
--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -2,49 +2,55 @@ import java.util.HashMap;
 
 public class RentalInfo {
 
-  public String statement(Customer customer) {
-    HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+	public String statement(Customer customer, HashMap<String, Movie> movies) {
+    	double totalAmount = 0;
+		int frequentEnterPoints = 0;
+		String result = "Rental Record for " + customer.getName() + "\n";
+		//Changing variable to meaningful name
+		for (MovieRental rental : customer.getRentals()) {
+			double thisAmount = 0;
 
-    double totalAmount = 0;
-    int frequentEnterPoints = 0;
-    String result = "Rental Record for " + customer.getName() + "\n";
-    for (MovieRental r : customer.getRentals()) {
-      double thisAmount = 0;
+			// determine amount for each movie
+			//Making Variables for calculation, readability and getting value one time in a loop
+			String currentRentalMovieCode = movies.get(rental.getMovieId()).getCode();
+			String currentRentalMovieTitle = movies.get(rental.getMovieId()).getTitle();
+			/*
+			 * Converted to switch case as "currentRentalMovieCode" is simple string and it
+			 * will be faster if types of codes increase in future, rather than if or
+			 * if-else and also each rental can have only one type of code
+			 */
+			switch (currentRentalMovieCode) {
+				case "regular" :
+					thisAmount = 2;
+					if (rental.getDays() > 2) {
+						thisAmount = ((rental.getDays() - 2) * 1.5) + thisAmount;
+					}
+					break;
+				case "new" :
+					thisAmount = rental.getDays() * 3;
+					break;
+				case "childrens" :
+					thisAmount = 1.5;
+					if (rental.getDays() > 3) {
+						thisAmount = ((rental.getDays() - 3) * 1.5) + thisAmount;
+					}
+					break;
+				
+			}
 
-      // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
-        thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
-        }
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
-        thisAmount = r.getDays() * 3;
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
-        thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-        }
-      }
+			//add frequent bonus points
+			frequentEnterPoints++;
+			// add bonus for a two day new release rental
+			if (currentRentalMovieCode == "new" && rental.getDays() > 2) frequentEnterPoints++;
 
-      //add frequent bonus points
-      frequentEnterPoints++;
-      // add bonus for a two day new release rental
-      if (movies.get(r.getMovieId()).getCode() == "new" && r.getDays() > 2) frequentEnterPoints++;
+			//print figures for this rental
+			result += "\t" + currentRentalMovieTitle + "\t" + thisAmount + "\n";
+			totalAmount = totalAmount + thisAmount;
+		}
+		// add footer lines
+		result += "Amount owed is " + totalAmount + "\n";
+		result += "You earned " + frequentEnterPoints + " frequent points\n";
 
-      //print figures for this rental
-      result += "\t" + movies.get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
-      totalAmount = totalAmount + thisAmount;
-    }
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentEnterPoints + " frequent points\n";
-
-    return result;
-  }
+		return result;
+	}
 }
