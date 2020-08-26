72db06208a495720ddb5f1a8e9f4fc1c6cba2a6c
André Santos <andrefmms23@gmail.com>
2020-08-26 20:48:02 +0200

    Reshaped Main to my taste: creating objects before using them + changed \n for System.lineSeparator()

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,30 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+    final String expected = "Rental Record for C. U. Stomer (ID: 1)" + System.lineSeparator() + "\tYou've Got Mail\t3.5"
+        + System.lineSeparator() + "\tMatrix\t2.0" + System.lineSeparator() + "Amount owed is 5.5"
+        + System.lineSeparator() + "You earned 2 frequent points" + System.lineSeparator();
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    // Creating the movies
+    Movie m1 = new Movie("You've Got Mail", Movie.MovieTag.REGULAR);
+    Movie m2 = new Movie("Matrix", Movie.MovieTag.REGULAR);
+    // The following 2 are not used but they were kepts as part of the original code
+    Movie m3 = new Movie("Cars", Movie.MovieTag.CHILDREN);
+    Movie m4 = new Movie("Fast & Furious X", Movie.MovieTag.NEW);
+
+    // Creating the movie rentals
+    MovieRental mr1 = new MovieRental(m1, 3);
+    MovieRental mr2 = new MovieRental(m2, 1);
+
+    // Creating the Customer
+    Customer c1 = new Customer("C. U. Stomer", Arrays.asList(mr1, mr2));
+
+    // Getting the rental information
+    String result = new RentalInfo().getCustomerStatement(c1);
 
     if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator()
+          + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
     }
 
     System.out.println("Success");
