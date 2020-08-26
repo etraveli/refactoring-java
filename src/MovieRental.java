9c2cffe4e6186f043595e8bad364bf234e4714a7
André Santos <andrefmms23@gmail.com>
2020-08-26 16:19:57 +0200

    Now considering the actual movie Id instead of descriptive String + updated var names for readibility + introduced setters

--- src/MovieRental.java
+++ src/MovieRental.java
@@ -1,17 +1,29 @@
 public class MovieRental {
-    private String movieId;
-    private int days;
+    // now considering the actual Movie "id"
+    private int movieId;
+    // "rentalDays" is more descriptive than "days"
+    private int rentalDays;
 
-    public MovieRental(String movieId, int days) {
+    public MovieRental(int movieId, int rentalDays) {
         this.movieId = movieId;
-        this.days = days;
+        this.rentalDays = rentalDays;
     }
 
-    public String getMovieId() {
+    // in case the Movie id needs to be changed/updated
+    public void setMovieId(int id) {
+        this.movieId = id;
+    }
+
+    public int getMovieId() {
         return movieId;
     }
 
-    public int getDays() {
-        return days;
+    // in case the number of rental days needs to be changed/updated
+    public void setRentalDays(int rentalDays) {
+        this.rentalDays = rentalDays;
+    }
+
+    public int getRentalDays() {
+        return rentalDays;
     }
 }
