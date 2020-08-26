67dad37375e4d127c0367b2fc7290ec4003100e3
André Santos <andrefmms23@gmail.com>
2020-08-26 15:57:53 +0200

    Implemented an enum to avoid entering free text on what was known as 'code' + introduced the concept of movie ID + created missing setters and new getters

--- src/Movie.java
+++ src/Movie.java
@@ -1,18 +1,53 @@
 public class Movie {
+
+    // created "nextId" to automatically increment the movie id at creation time.
+    // created "id" to identify a given movie uniquely.
+    // introduced "tag" instead of "code" to avoid interpretation issues.
+    private static int nextId = 1;
+    private int id;
     private String title;
-    private String code;
+    private MovieTag tag;
 
-    public Movie(String title, String code) {
+    /*
+     * "MovieTag" includes catching words intended for the movies. Formerly known as
+     * "code".
+     */
+    public enum MovieTag {
+        NEW, CHILDREN, REGULAR
+    }
 
+    public Movie(String title, MovieTag tag) {
+        this.id = nextId;
         this.title = title;
-        this.code = code;
+        this.tag = tag;
+        this.nextId++;
+    }
+
+    // used at object creation time
+    public static int getNextId() {
+        return nextId;
+    }
+
+    // no setter as it is automatically incremented
+    public int getId() {
+        return id;
+    }
+
+    // in case the title needs to be changed/updated
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public String getCode() {
-        return code;
+    // in case the movie tag needs tbe changed/updated
+    public void setTag(MovieTag tag) {
+        this.tag = tag;
+    }
+
+    public MovieTag getTag() {
+        return tag;
     }
 }
