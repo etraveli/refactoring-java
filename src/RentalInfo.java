7e44e428aae0953efe7d1c9c20e38b3351831ffb
Md Zubayer Alam <zubayer.ie@gmail.com>
2020-09-22 19:05:22 +0200

    Refactored

--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -1,62 +1,69 @@
+import java.util.List;
+
 public class RentalInfo {
+	final static String ERROR_MESSAGE = "Error";
+	final static double AMOUNT_REGULAR = 2.0;
+	final static double AMOUNT_CHILDREN = 1.5;
 	
-  final static String ERROR_MESSAGE = "Error";
-  
-  // We make this method static as we don't need to create any object of this class.
-  public static String statement(Customer customer) {
-	// Check some null points 
-	if (customer == null) return ERROR_MESSAGE;
-	if (customer.getRentals() == null) return ERROR_MESSAGE;
-
-    double totalAmount = 0;
-    int frequentEnterPoints = 0;
-    
-    String result = "Rental Record for " + customer.getName() + "\n";
-    
-    for (MovieRental r : customer.getRentals()) {
-      Movie rentedMovie = MovieDataStorage.getMovieById(r.getMovieId());
-      if (rentedMovie == null) continue;
-      
-      double thisAmount = 0;
-
-      // determine amount for each movie   
-      
-      // A series of nested if else is replaces with switch statement. It is faster and improves readability.
-      switch(rentedMovie.getCode()) {
-    	case REGULAR:
-    		thisAmount = 2;
-    		if (r.getDays() > 2) {
-    			thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
-            }
-    		break;
-          
-    	case CHILDREN:
-    		thisAmount = 1.5;
-    		if (r.getDays() > 3) {
-    			thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-    		}
-    		break;
-           
-    	case NEW:
-    		thisAmount = r.getDays() * 3;
-    		// add bonus for more than two days new release rental
-    		if (r.getDays() > 2) {
-    			frequentEnterPoints++;
-    		}
-      }
-
-      // add frequent bonus points
-      frequentEnterPoints++;     
-
-      // print figures for this rental
-      result += "\t" + rentedMovie.getTitle() + "\t" + thisAmount + "\n";
-      totalAmount = totalAmount + thisAmount;
-    }
-    
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentEnterPoints + " frequent points\n";
-
-    return result;
-  }
+	// We made this basically a util class
+	// Method is broken down into smaller unit method.
+	// Very easy to read
+	
+	public static String statement(Customer customer) {
+		if (customer == null || customer.getRentals() == null) return ERROR_MESSAGE;
+		String result = "Rental Record for " + customer.getName() +  "\n";
+		
+		for(MovieRental rental : customer.getRentals()) {
+			if (MovieDataStorage.getMovieById(rental.getMovieId()) == null) continue;
+			result += "\t" + statement(rental);
+		}
+		
+		// add footer lines
+		result += "Amount owed is " + calculateAmount(customer.getRentals()) + "\n";
+		result += "You earned " + calculateFrequentEnterPoint(customer.getRentals()) + " frequent points\n";  
+		return result;
+	}
+	
+	private static double calculateAmount(MovieRental rental) {
+		Movie rentedMovie = MovieDataStorage.getMovieById(rental.getMovieId());
+		if (MovieDataStorage.getMovieById(rental.getMovieId()) == null) return 0;
+		
+		switch(rentedMovie.getCode()) {
+	    	case REGULAR:
+	    		return (rental.getDays() > 2)? ((rental.getDays() - 2) * 1.5) + AMOUNT_REGULAR : AMOUNT_REGULAR;
+	          
+	    	case CHILDREN:
+	    		return (rental.getDays() > 3)? ((rental.getDays() - 3) * 1.5) + AMOUNT_CHILDREN : AMOUNT_CHILDREN;
+	           
+	    	case NEW:
+	    		return rental.getDays() * 3;
+	    		
+	    	default: 
+	    		return 0;   		
+		}
+	}
+	
+	private static int calculateFrequentEnterPoint(MovieRental rental) {
+		if (MovieDataStorage.getMovieById(rental.getMovieId()) == null) return 0;
+		switch(MovieDataStorage.getMovieById(rental.getMovieId()).getCode()) {
+			case NEW:
+				// extra bonus for more than two days new release rental
+				return (rental.getDays() > 2)? 2 : 1;
+				
+			default:
+				return 1;			
+		}	
+	}
+	
+	private static String statement(MovieRental rental) {
+		return MovieDataStorage.getMovieById(rental.getMovieId()).getTitle() + "\t" + calculateAmount(rental) + "\n";	
+	}
+	
+	private static double calculateAmount(List<MovieRental> rentals) {
+		return rentals.stream().map(x -> calculateAmount(x)).mapToDouble(Double::doubleValue).sum();
+	}
+	
+	private static int calculateFrequentEnterPoint(List<MovieRental> rentals) {
+		return rentals.stream().map(x -> calculateFrequentEnterPoint(x)).mapToInt(Integer::intValue).sum();
+	}
 }
