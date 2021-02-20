0335bff5919191ab01e41e81c0cfbbbb11246e0d
Ramapriya Navalpakkam <ramapriya@enit.se>
2021-02-20 17:37:14 +0100

    seperated concerns

--- src/main/RentalInfo.java
+++ src/main/RentalInfo.java
@@ -8,8 +8,6 @@ import java.util.HashMap;
 
 public class RentalInfo {
 
-
-  // A static final cuz we want to intialize movies hashmap once and not change it.
   private static final HashMap<String, Movie> movies = new HashMap<String, Movie>() {{
     put("F001", new Movie("You've Got Mail", MovieCode.REGULAR));
     put("F002", new Movie("Matrix", MovieCode.REGULAR));
@@ -25,40 +23,56 @@ public class RentalInfo {
     StringBuilder result = new StringBuilder("Rental Record for " + customer.getName() + "\n");
     for (MovieRental r : customer.getRentals()) {
       double thisAmount = 0;
-      // good to define a variable accessing movies once rather than on ever condition. Makes code more readable. If its a database call makes positive difference.
-      MovieCode movieCode = movies.get(r.getMovieId()).getCode();
-      // determine amount for each movie
-      if (movieCode.equals(MovieCode.REGULAR)) {
-        thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
-        }
-      }
-      if (movieCode.equals(MovieCode.NEW)) {
-        thisAmount = r.getDays() * 3;
-        // add bonus for a two day new release rental
-        //moved here to avoid repetition of checking conditions
-        if(r.getDays() > 2)
-          frequentEnterPoints++;
-      }
-      if (movieCode.equals(MovieCode.CHILDRENS)) {
-        thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-        }
-      }
-      //add frequent bonus points
-      frequentEnterPoints++;
 
+      MovieCode movieCode = movies.get(r.getMovieId()).getCode();
+
+      // compute amount for each movie
+      thisAmount = computeAmount(movieCode, r.getDays());
+
+      //compute bonus
+      frequentEnterPoints = computeBonusPoints(movieCode,r.getDays(),frequentEnterPoints);
 
       //print figures for this rental
       result.append("\t").append(movies.get(r.getMovieId()).getTitle()).append("\t").append(thisAmount).append("\n");
+
       totalAmount = totalAmount + thisAmount;
     }
+
     // add footer lines
     result.append("Amount owed is ").append(totalAmount).append("\n");
     result.append("You earned ").append(frequentEnterPoints).append(" frequent points\n");
 
     return result.toString();
   }
+
+
+  public static double computeAmount(MovieCode movieCode, int daysRented) {
+    double movieRentalAmount = 0;
+    if (movieCode.equals(MovieCode.REGULAR)) {
+      movieRentalAmount = 2;
+      if (daysRented > 2) {
+        movieRentalAmount = ((daysRented - 2) * 1.5) + movieRentalAmount;
+      }
+    }
+    if (movieCode.equals(MovieCode.NEW)) {
+      movieRentalAmount = daysRented * 3;
+    }
+    if (movieCode.equals(MovieCode.CHILDRENS)) {
+      movieRentalAmount = 1.5;
+      if (daysRented > 3) {
+        movieRentalAmount = ((daysRented - 3) * 1.5) + movieRentalAmount;
+      }
+    }
+    return movieRentalAmount;
+  }
+
+  public static int computeBonusPoints(MovieCode movieCode, int daysRented, int frequentEnterPoints){
+    // add bonus for a two day new release rental
+    if (movieCode.equals(MovieCode.NEW) && daysRented > 2)
+      frequentEnterPoints++;
+    //add bonus for the rental
+    return ++frequentEnterPoints;
+  }
+
+
 }
