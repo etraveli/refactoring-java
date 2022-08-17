8aedd376787a86dec65f20d3a6e6dd454bf59151
cristianstocco <stykky@NewWorld.local>
2022-08-17 12:18:32 +0200

    refactor test case provided

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,9 @@ import service.RentalInfoService;
  * MISSION: it creates an information slip about movie rentals.
  */
 public class Main {
+	
+	private final Customer CUSTOMER_TEST = new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1)));
+	private final String CUSTOMER_TEST_STATEMENT = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
 	/*
 	 * MAIN
@@ -23,9 +26,8 @@ public class Main {
 	 * PRIVATE METHODS
 	 */
 	private void execute() {
-		String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
-
-		String result = new RentalInfoService().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+		String expected = CUSTOMER_TEST_STATEMENT;
+		String result = new RentalInfoService().statement( CUSTOMER_TEST );
 
 		if (!result.equals(expected)) {
 			throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
