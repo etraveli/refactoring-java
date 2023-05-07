a488ffb69af685b068368a2692ab9052bcd36a9e
akh-gup <er.akhilgupta92@gmail.com>
2023-05-07 13:21:33 +0200

    Bonus file annotation update to Component from service

--- src/main/java/se/etraveli/movie/rentals/business/bonus/FrequentEnterPointsImpl.java
+++ src/main/java/se/etraveli/movie/rentals/business/bonus/FrequentEnterPointsImpl.java
@@ -1,9 +1,9 @@
 package se.etraveli.movie.rentals.business.bonus;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import se.etraveli.movie.rentals.model.MovieCode;
 
-@Service
+@Component
 public class FrequentEnterPointsImpl implements FrequentEnterPoints {
 
     @Override
