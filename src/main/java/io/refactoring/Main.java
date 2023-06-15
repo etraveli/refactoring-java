0eb010e4822bd55a91f2ca3275605e4bdd330756
Mathews <robins.mathews@capgemini.com>
2023-06-16 01:06:44 +0200

    Refactoring: Updated the main class with proper response message for different scenarios

--- src/main/java/io/refactoring/Main.java
+++ src/main/java/io/refactoring/Main.java
@@ -13,24 +13,27 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Main {
 
-  private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-  public static void main(String[] args) {
-    var configurableApplicationContext = SpringApplication.run(Main.class, args);
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
-    String result = "";
+    public static void main(String[] args) {
+        var configurableApplicationContext = SpringApplication.run(Main.class, args);
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+        String result;
+        String message;
 
-    try {
-      result = ((IRentalInfo) configurableApplicationContext.getBean("rentalInfo")).statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+        try {
+            result = ((IRentalInfo) configurableApplicationContext.getBean("rentalInfo")).statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
 
-      if (!result.equals(expected)) {
-        throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
-      }
+            if (!result.equals(expected)) {
+                message = "Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result;
+            } else {
+                message = "Success";
+            }
+        } catch (Exception e) {
+            message = "Exception in processing rental info with message " + e.getMessage();
+        }
 
-      log.info("=======Success======");
-    }catch (Exception e){
-      log.error("Exception in processing rental info with message {}", e.getMessage());
+        log.info("\nOUTPUT\n{}", message);
     }
-  }
 
 }
