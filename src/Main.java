56fe29d8413d4c49d1f77d5f2c69aa575c6a5887
VIVAR16 <vishnu.vardhan.revalapalli@ingka.com>
2023-09-29 17:01:05 +0200

    Refactored code, created folder structure and added details in ReadMe.md file

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
+import models.Customer;
+import models.MovieRental;
+import service.RentalInfo;
+
 import java.util.Arrays;
 
 public class Main {
 
-  public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+    public static void main(String[] args) {
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+        Customer customer = new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1)));
+        RentalInfo rentalInfo = new RentalInfo();
+        String result = rentalInfo.statement(customer);
 
-    if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+        if (!result.equals(expected)) {
+            throw new AssertionError("Test failed. Expected output:\n" + expected + "\nActual output:\n" + result);
+        }
+        System.out.println("Test passed: Success");
+        System.out.println("Information Slip: "+ result);
     }
-
-    System.out.println("Success");
-  }
 }
