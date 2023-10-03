8960c258ef0e3306142a1daad164abd753ffe99d
VIVAR16 <vishnu.vardhan.revalapalli@ingka.com>
2023-10-03 08:18:05 +0200

    code cleanup

--- src/main/java/com/etraveli/movie/rental/service/controller/MovieRentalController.java
+++ src/main/java/com/etraveli/movie/rental/service/controller/MovieRentalController.java
@@ -5,7 +5,6 @@ import com.etraveli.movie.rental.service.response.RentalDetailsResponse;
 import com.etraveli.movie.rental.service.service.MovieRentalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
