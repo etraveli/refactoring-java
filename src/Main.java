f116594df8f6f3e1bfc9939a724b42dbc85fcc28
Daniel Crosby <danielcrosby844@gmail.com>
2023-11-16 18:27:44 +0100

    Refactored RentalInfo into Statement object

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,16 @@ public class Main {
   public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    Customer customer = new Customer("C. U. Stomer",
+        Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1)));
 
-    if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+    Statement statement = new Statement(customer);
+
+    if (!statement.toString().equals(expected)) {
+      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator()
+          + System.lineSeparator() + "Got: " + System.lineSeparator() + statement.toString());
     }
 
-    System.out.println("Success");
+    System.out.println(statement);
   }
 }
