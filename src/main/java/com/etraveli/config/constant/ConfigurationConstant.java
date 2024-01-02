64ab2822afd51a036c86104a2dc2c437615567a9
sahan89 <sahan1989@gmail.com>
2024-01-02 20:25:13 +0100

    Swagger RestAPI document configuration.

---
+++ src/main/java/com/etraveli/config/constant/ConfigurationConstant.java
@@ -0,0 +1,7 @@
+package com.etraveli.config.constant;
+
+public class ConfigurationConstant {
+    public static final String SWAGGER_TITLE = "Refactoring Java";
+    public static final String SWAGGER_VERSION = "1.0";
+    public static final String SWAGGER_DESCRIPTION = "Refactoring Java project for Etraveli";
+}
