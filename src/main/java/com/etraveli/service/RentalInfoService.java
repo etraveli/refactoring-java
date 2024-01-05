6047e663c11fdfb6302f5477020aed396b08e5fc
sahan89 <sahan1989@gmail.com>
2024-01-05 17:01:03 +0100

    Default data insert to the database in runtime.

--- src/main/java/com/etraveli/service/RentalInfoService.java
+++ src/main/java/com/etraveli/service/RentalInfoService.java
@@ -1,11 +1,15 @@
 package com.etraveli.service;
 
+import com.etraveli.modal.MovieRental;
 import com.etraveli.modal.request.MovieRentalRequest;
 import com.etraveli.modal.response.MovieRentalResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface RentalInfoService {
     String statement(Long customer);
     MovieRentalResponse createMovieRental(MovieRentalRequest movieRentalRequest);
+    List<MovieRental> getAllMovieRentals();
 }
--- src/main/java/com/etraveli/service/impl/MovieRentalServiceImpl.java
+++ src/main/java/com/etraveli/service/impl/MovieRentalServiceImpl.java
@@ -18,6 +18,8 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Service
@@ -136,6 +138,11 @@ public class MovieRentalServiceImpl implements RentalInfoService {
 
         logger.info("--EXIT--createMovieRental--");
         return this.mapEntityToResponse(savedMovieRental);
+    }
+
+    @Override
+    public List<MovieRental> getAllMovieRentals() {
+        return this.movieRentalRepository.findAll();
     }
 
     private MovieRental mapRequestToEntity(MovieRentalRequest movieRentalRequest) {
