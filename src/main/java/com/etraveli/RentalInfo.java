a84f0f5281e1b1afa42d5340ab1866357fe9a0cc
jjunior <juniorn@seznam.cz>
2024-01-17 21:14:16 +0100

    Move logic dependent on movie code to appropriately created enum and utilize it. Also utilize the MovieRepository.

--- src/main/java/com/etraveli/RentalInfo.java
+++ src/main/java/com/etraveli/RentalInfo.java
@@ -1,46 +1,30 @@
 package com.etraveli;
 
+import com.etraveli.model.Customer;
+import com.etraveli.model.Movie;
+import com.etraveli.model.MovieRental;
+import com.etraveli.repository.MovieRepository;
+
 import java.util.HashMap;
 
 public class RentalInfo {
+  private MovieRepository movieRepository;
+
+  public RentalInfo(MovieRepository movieRepository){
+    this.movieRepository = movieRepository;
+  }
 
   public String statement(Customer customer) {
-    HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
-
     double totalAmount = 0;
     int frequentEnterPoints = 0;
-    String result = "Rental Record for " + customer.getName() + "\n";
-    for (MovieRental r : customer.getRentals()) {
-      double thisAmount = 0;
-
-      // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
-        thisAmount = 2;
-        if (r.getDays() > 2) {
-          thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
-        }
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
-        thisAmount = r.getDays() * 3;
-      }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
-        thisAmount = 1.5;
-        if (r.getDays() > 3) {
-          thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
-        }
-      }
-
-      //add frequent bonus points
-      frequentEnterPoints++;
-      // add bonus for a two day new release rental
-      if (movies.get(r.getMovieId()).getCode() == "new" && r.getDays() > 2) frequentEnterPoints++;
+    String result = "Rental Record for " + customer.name() + "\n";
+    for (MovieRental rental : customer.rentals()) {
+      Movie movie = movieRepository.getMovieById(rental.movieId());
+      double thisAmount  = movie.code().amount(rental.days());
+      frequentEnterPoints += movie.code().frequentEnterPoints(rental.days());
 
       //print figures for this rental
-      result += "\t" + movies.get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
+      result += "\t" + movie.title() + "\t" + thisAmount + "\n";
       totalAmount = totalAmount + thisAmount;
     }
     // add footer lines
---
+++ src/main/java/com/etraveli/model/MovieCode.java
@@ -0,0 +1,26 @@
+package com.etraveli.model;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public enum MovieCode {
+    REGULAR(days -> days > 2 ? 2d : ((days - 2) * 1.5) + 2, days -> 1),
+    CHILDRENS(days -> days > 3 ? 1.5 : ((days - 3) * 1.5) + 1.5, days -> 1),
+    NEW(days -> days * 3d, days -> days > 2 ? 2 : 1);
+
+    final Function<Integer, Double> amountEquation;
+    final UnaryOperator<Integer> pointsEquation;
+
+    MovieCode(Function<Integer, Double> amountEquation, UnaryOperator<Integer> pointsEquation) {
+        this.amountEquation = amountEquation;
+        this.pointsEquation = pointsEquation;
+    }
+
+    public double amount(int days){
+        return this.amountEquation.apply(days);
+    }
+
+    public int frequentEnterPoints(int days){
+        return this.pointsEquation.apply(days);
+    }
+}
