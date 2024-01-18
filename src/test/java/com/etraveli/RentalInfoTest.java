130b27520f3c6a5f7aa5007eb54e0215b1bc406f
jjunior <juniorn@seznam.cz>
2024-01-18 01:46:21 +0100

    Remove unused import.

--- src/test/java/com/etraveli/RentalInfoTest.java
+++ src/test/java/com/etraveli/RentalInfoTest.java
@@ -7,7 +7,6 @@ import com.etraveli.repository.InitialMovieRepository;
 import com.etraveli.repository.MovieRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
